c1966637039177348ac6f15d9690ba9152217d8e
Edison Cresentino <Edison.Cresentino@student.unmer.ac.id>
2025-05-01 13:39:42 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('Profile.show');
     }
 
     public function blank()
